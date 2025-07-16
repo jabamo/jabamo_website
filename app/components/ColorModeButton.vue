89b4630ad7b2d9030ce6225973076047fb464522
Jona-David Bastian <jona-david.bastian@mni.thm.de>
2025-07-16 16:51:21 +0200

    Fix: Fix hydration mismatch in ColorModeButton.vue.

--- app/components/ColorModeButton.vue
+++ app/components/ColorModeButton.vue
@@ -4,11 +4,20 @@
     class="flex p-2 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
     :aria-label="colorMode.value === 'dark' ? 'Zum hellen Modus wechseln' : 'Zum dunklen Modus wechseln'"
   >
-    <Icon 
-      :name="colorMode.value === 'dark' ? 'tabler:sun' : 'tabler:moon'"
-      size="20"
-      class="items-center justify-center text-gray-700 dark:text-gray-300"
-    />
+    <ClientOnly>
+      <Icon
+          :name="colorMode.value === 'dark' ? 'tabler:sun' : 'tabler:moon'"
+          size="20"
+          class="items-center justify-center text-gray-700 dark:text-gray-300"
+      />
+      <template #fallback>
+        <Icon
+            name="tabler:moon"
+            size="20"
+            class="items-center justify-center text-gray-700 dark:text-gray-300"
+        />
+      </template>
+    </ClientOnly>
   </button>
 </template>
 
